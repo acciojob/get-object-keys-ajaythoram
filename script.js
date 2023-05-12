4824b737632aacbeefdeefc59adb1ceb8e88a682
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-12 18:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const student = {
+ name : "Ajay";
+}
+Object.prototype.getKeys = function(){
+ return Object.keys(this);
+}
+const keys = stufent.getKeys();
+console.log(keys);
+
